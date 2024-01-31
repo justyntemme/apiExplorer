525f072289717e9b3797d4b7782188d63371b20c
justyn temme <justyntemme@gmail.com>
2024-01-31 11:20:14 -0500

    use correct auth url's for each api automatically

--- main.py
+++ main.py
@@ -3,54 +3,83 @@ import logging
 import requests
 import os
 import csv
-from typing import Tuple, Optional
 import argparse
+from typing import Tuple, Optional
+from urllib.parse import urlparse, urlunparse
 
-# Setup basic logging
 logging.basicConfig(level=logging.INFO)
 
 
-def prisma_cwp_login(
+def get_base_url(full_url: str) -> str:
+    parsed_url = urlparse(full_url)
+    # Construct the base URL by keeping only the scheme, netloc, and path (up to the second last segment)
+    path_segments = parsed_url.path.split("/")
+    base_path = "/".join(path_segments[:-2])  # Remove the last two segments
+    base_url = urlunparse((parsed_url.scheme, parsed_url.netloc, base_path, "", "", ""))
+    return base_url
+
+
+def return_hostname(url: str) -> str:
+    parsed_url = urlparse(url)
+    return parsed_url.hostname
+
+
+def is_twistlock_in_url(url: str) -> bool:
+    """
+    Checks if 'twistlock' is in the hostname of the URL.
+
+    Parameters:
+    url (str): The URL to be checked.
+
+    Returns:
+    bool: True if 'twistlock' is in the hostname, False otherwise.
+    """
+    parsed_url = urlparse(url)
+    hostname = parsed_url.hostname
+    return "twistlock" in hostname if hostname else False
+
+
+def prisma_login(
     base_url: str, api_version: str, access_key: str, secret_key: str
 ) -> Tuple[int, dict]:
-    apiURL = f"{base_url}/api/v{api_version}/authenticate"
+    base_url = get_base_url(base_url)
+    if is_twistlock_in_url(base_url):
+        apiURL = f"{base_url}/v1/authenticate"
+    else:
+        apiURL = f"{base_url}/v1/login"
     headers = {
         "accept": "application/json; charset=UTF-8",
         "content-type": "application/json",
     }
     body = {"username": access_key, "password": secret_key}
-    logging.info("Generating PRISMA token using endpoint: %s", apiURL)
+    logging.info("Generating token using endpoint: %s", apiURL)
     response = requests.post(apiURL, headers=headers, json=body, timeout=60)
     if response.status_code == 200:
         data = json.loads(response.text)
         logging.info("Token acquired")
         return 200, data
-    logging.error(
-        "Unable to acquire PRISMA token with error code: %s", response.status_code
-    )
+    logging.error("Unable to acquire token with error code: %s", response.status_code)
     return response.status_code, None
 
 
 def make_request(
-    base_url: str,
+    url: str,
     api_version: str,
-    api_endpoint: str,
     access_token: str,
     content_type: str,
-    method: str = "GET",
-    data: Optional[dict] = None,
+    method: str,
+    data: Optional[dict],
 ) -> Tuple[int, Optional[str]]:
-    apiURL = f"{base_url}/api/v{api_version}{api_endpoint}"
     headers = {
         "Content-Type": content_type,
         "Authorization": f"Bearer {access_token}",
     }
-    logging.info(f"Making {method} request to {apiURL}")
+    logging.info(f"Making {method} request to {url}")
 
     if method.upper() == "GET":
-        response = requests.get(apiURL, headers=headers)
+        response = requests.get(url, headers=headers)
     elif method.upper() == "POST":
-        response = requests.post(apiURL, headers=headers, json=data)
+        response = requests.post(url, headers=headers, json=data)
     else:
         logging.error(f"Invalid request method: {method}")
         return 405, None
@@ -59,7 +88,7 @@ def make_request(
         return 200, response.text
     else:
         logging.error(
-            f"Failed to query endpoint {apiURL} with status code: {response.status_code}"
+            f"Failed to query endpoint {url} with status code: {response.status_code}"
         )
         return response.status_code, None
 
@@ -68,10 +97,13 @@ def main():
     parser = argparse.ArgumentParser(
         description="Script to interact with Prisma Cloud API."
     )
-    parser.add_argument("--endpoint", type=str, required=True, help="API endpoint")
     parser.add_argument(
         "--type", type=str, required=True, help="Request Type (GET/POST/PUT)"
     )
+    parser.add_argument(
+        "--url", type=str, required=True, help="Request URL overrides default"
+    )
+
     args = parser.parse_args()
 
     accessKey = os.environ.get("PC_IDENTITY")
@@ -80,27 +112,18 @@ def main():
     api_version = "1"
     csv.field_size_limit(10000000)
 
-    pcToken = prisma_cwp_login(base_url, api_version, accessKey, accessSecret)
+    pcToken = prisma_login(args.url, api_version, accessKey, accessSecret)
+
     if pcToken[0] != 200:
         exit()
     logging.info("Token: %s", pcToken[1]["token"])
 
     pcData = make_request(
-        base_url, api_version, args.endpoint, pcToken[1]["token"], "text/csv", args.type
+        args.url, api_version, pcToken[1]["token"], "text/csv", args.type, None
     )
     if pcData[0] != 200:
         exit()
-
-    csv_reader = csv.reader(pcData[1].splitlines())
-    desired_column_index = 4
-    unique_values = set()
-
-    for row in csv_reader:
-        if row and len(row) > desired_column_index:
-            unique_values.add(row[desired_column_index])
-
-    for value in unique_values:
-        print(value)
+    print(pcData[1])
 
 
 if __name__ == "__main__":
